4e8e4e19fa7ecaf9e655a04b4e0be9dedd79ba12
Deny Pradana <pradana.deny@gmail.com>
2020-04-02 08:39:16 +0700

    Update login.py

--- login.py
+++ login.py
@@ -3,6 +3,8 @@ import json
 # Variabel user_data merupakan variabel JSON yang berisi daftar user id telegram user yang mempunyai
 # akses ke sistem. Untuk menambahkan user telegram anda, anda bisa mengakses https://botostore.com/c/getmyid_bot/
 # kemudian tambahkan user id dan password anda kedalam JSON user_data sesuai format yang ada
+#
+# Note : Jangan menggunakan password yang sama dengan user lain !
 user_data = '[{"user_id":"463662008","password":"KucingImut"},{"user_id":"463662009","password":"KUCINGGARONG"}]'
 user_array = json.loads(user_data)
 
